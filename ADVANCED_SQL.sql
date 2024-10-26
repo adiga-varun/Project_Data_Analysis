b733df24730d50037b8472c1fe3a043c77813938
adiga-varun <varunadiga@gmx.de>
2024-10-26 21:45:01 +0530

    Create ADVANCED_SQL.sql

---
+++ ADVANCED_SQL.sql
@@ -0,0 +1,7 @@
+echo "# Project_Data_Analysis" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:adiga-varun/Project_Data_Analysis.git
+git push -u origin main
